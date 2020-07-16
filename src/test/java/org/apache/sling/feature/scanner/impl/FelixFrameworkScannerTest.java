231e50ecff885ae38af9453773a95800ededab16
David Bosschaert <bosschae@adobe.com>
2020-07-16 10:19:49 +0100

    SLING-9425 Analyser fails when bundle requires Java 11
    Launch a real framework to obtain framework exports and capabilities.

--- src/test/java/org/apache/sling/feature/scanner/impl/FelixFrameworkScannerTest.java
+++ src/test/java/org/apache/sling/feature/scanner/impl/FelixFrameworkScannerTest.java
@@ -16,49 +16,83 @@
  */
 package org.apache.sling.feature.scanner.impl;
 
+import org.apache.felix.framework.Felix;
 import org.apache.sling.feature.ArtifactId;
 import org.apache.sling.feature.builder.ArtifactProvider;
 import org.apache.sling.feature.scanner.BundleDescriptor;
+import org.apache.sling.feature.scanner.PackageInfo;
 import org.junit.Test;
+import org.osgi.framework.Constants;
+import org.osgi.resource.Capability;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class FelixFrameworkScannerTest {
     @Test
     public void testGetFrameworkProperties() throws Exception {
-        URL url = getClass().getResource("/test-framework.jar");
+        URL url = getFelixFrameworkJar();
         File fwFile = new File(url.toURI());
 
         FelixFrameworkScanner ffs = new FelixFrameworkScanner();
 
+        String additionalVersions = getAdditionalVersionNumbers();
+
         Map<String,String> kvmap = new HashMap<>();
         Map<String,String> props = ffs.getFrameworkProperties(kvmap, fwFile.toURI().toURL());
-        assertEquals("osgi.service; objectClass:List<String>=org.osgi.service.resolver.Resolver; "
+        String expected = "osgi.service; objectClass:List<String>=org.osgi.service.resolver.Resolver; "
                     + "uses:=org.osgi.service.resolver, "
                 + "osgi.service; objectClass:List<String>=org.osgi.service.startlevel.StartLevel; "
                     + "uses:=org.osgi.service.startlevel, "
                 + "osgi.service; objectClass:List<String>=org.osgi.service.packageadmin.PackageAdmin; "
                     + "uses:=org.osgi.service.packageadmin , "
                 + "osgi.ee; osgi.ee=\"OSGi/Minimum\"; version:List<Version>=\"1.0,1.1,1.2\", "
-                + "osgi.ee; osgi.ee=\"JavaSE\"; version:List<Version>=\"1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8\", "
-                + "osgi.ee; osgi.ee=\"JavaSE/compact1\"; version:List<Version>=\"1.8\", "
-                + "osgi.ee; osgi.ee=\"JavaSE/compact2\"; version:List<Version>=\"1.8\", "
-                + "osgi.ee; osgi.ee=\"JavaSE/compact3\"; version:List<Version>=\"1.8\" ", props.get("org.osgi.framework.system.capabilities"));
+                + "osgi.ee; osgi.ee=\"JavaSE\"; version:List<Version>=\"1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8"
+                    + additionalVersions + "\", "
+                + "osgi.ee; osgi.ee=\"JavaSE/compact1\"; version:List<Version>=\"1.8"
+                    + additionalVersions + "\", "
+                + "osgi.ee; osgi.ee=\"JavaSE/compact2\"; version:List<Version>=\"1.8"
+                    + additionalVersions + "\", "
+                + "osgi.ee; osgi.ee=\"JavaSE/compact3\"; version:List<Version>=\"1.8"
+                    + additionalVersions + "\"";
+        String actual = props.get("org.osgi.framework.system.capabilities");
+
+        // Remove spaces
+        assertEquals(expected.replaceAll("\\s",""), actual.replaceAll("\\s",""));
+    }
+
+    private String getAdditionalVersionNumbers() {
+        StringBuilder sb = new StringBuilder();
+
+        String javaVersion = System.getProperty("java.specification.version");
+        if (!javaVersion.startsWith("1.")) {
+            int curVersion = Integer.parseInt(javaVersion);
+
+            for (int i=9; i<=curVersion; i++) {
+                sb.append(',');
+                sb.append(i);
+            }
+        }
+
+        return sb.toString();
     }
 
     @Test
     public void testGetFrameworkExports() throws Exception {
-        URL fwFile = getClass().getResource("/test-framework.jar");
-
+        URL fwFile = getFelixFrameworkJar();
         FelixFrameworkScanner ffs = new FelixFrameworkScanner();
 
         Map<String,String> kvmap = new HashMap<>();
+        kvmap.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, "org.foo.bar");
+        kvmap.put(Constants.FRAMEWORK_SYSTEMCAPABILITIES_EXTRA, "ding.dong;ding.dong=\"yeah!\"");
 
         BundleDescriptor bundleDescriptor = ffs.scan(new ArtifactId("org.apache.felix",
                 "org.apache.felix.framework",
@@ -70,7 +104,26 @@ public class FelixFrameworkScannerTest {
                     }
                 });
 
-        assertFalse(bundleDescriptor.getExportedPackages().isEmpty());
-        assertFalse(bundleDescriptor.getCapabilities().isEmpty());
+        Set<PackageInfo> exportedPackages = bundleDescriptor.getExportedPackages();
+        assertFalse(exportedPackages.isEmpty());
+        boolean foundFooBar = false;
+        for (PackageInfo pi : exportedPackages) {
+            if (pi.getName().equals("org.foo.bar"))
+                foundFooBar = true;
+        }
+        assertTrue(foundFooBar);
+
+        Set<Capability> providedCaps = bundleDescriptor.getCapabilities();
+        assertFalse(providedCaps.isEmpty());
+        boolean foundDingDong = false;
+        for (Capability cap : providedCaps) {
+            if (cap.getNamespace().equals("ding.dong") && "yeah!".equals(cap.getAttributes().get("ding.dong")))
+                foundDingDong = true;
+        }
+        assertTrue(foundDingDong);
+    }
+
+    private URL getFelixFrameworkJar() throws IOException {
+        return FelixFrameworkScanner.getClasspathForClass(Felix.class).toURI().toURL();
     }
 }
